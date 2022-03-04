51217ad7cf0070839df0b3a9ba750de74f8db69a
JuanFTP <juanftp10@gmail.com>
2022-03-04 00:15:59 -0600

    Diseño base, primer iteración

--- src/utils/hooks/useLatestAPI.js
+++ src/utils/hooks/useLatestAPI.js
@@ -1,36 +1,36 @@
-import { useState, useEffect } from 'react';
-import { API_BASE_URL } from '../constants';
+import { useState, useEffect } from "react";
+import { API_BASE_URL } from "../constants";
 
 const INITIAL_API_METADATA = { ref: null, isLoading: true };
 
 export function useLatestAPI() {
-  const [apiMetadata, setApiMetadata] = useState(() => INITIAL_API_METADATA);
+	const [apiMetadata, setApiMetadata] = useState(() => INITIAL_API_METADATA);
 
-  useEffect(() => {
-    const controller = new AbortController();
+	useEffect(() => {
+		const controller = new AbortController();
 
-    async function getAPIMetadata() {
-      try {
-        setApiMetadata(INITIAL_API_METADATA);
+		async function getAPIMetadata() {
+			try {
+				setApiMetadata(INITIAL_API_METADATA);
 
-        const response = await fetch(API_BASE_URL, {
-          signal: controller.signal,
-        });
-        const { refs: [{ ref } = {}] = [] } = await response.json();
+				const response = await fetch(API_BASE_URL, {
+					signal: controller.signal,
+				});
+				const { refs: [{ ref } = {}] = [] } = await response.json();
 
-        setApiMetadata({ ref, isLoading: false });
-      } catch (err) {
-        setApiMetadata({ ref: null, isLoading: false });
-        console.error(err);
-      }
-    }
+				setApiMetadata({ ref, isLoading: false });
+			} catch (err) {
+				setApiMetadata({ ref: null, isLoading: false });
+				console.error(err);
+			}
+		}
 
-    getAPIMetadata();
+		getAPIMetadata();
 
-    return () => {
-      controller.abort();
-    };
-  }, []);
+		return () => {
+			controller.abort();
+		};
+	}, []);
 
-  return apiMetadata;
+	return apiMetadata;
 }
